87bfff96f0bc0cee97cb9ac2b909d077c2c461f4
Mateo Salas <masalas787@misena.edu.co>
2023-05-10 21:59:27 -0500

    componente nuevo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//angular material
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
+//component
 import { LoginComponent } from './component/login/login.component';
 
 @NgModule({
@@ -17,6 +24,9 @@ import { LoginComponent } from './component/login/login.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSlideToggleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  form: FormGroup; 
 
+
+
+  constructor(private fb: FormBuilder ){
+   this.form = this.fb.group({
+    usuario:['', Validators.required],
+    password:['', Validators.required],
+
+   })
+  }
+
+  ngOnInit(): void{}
+
+  Ingresar(){
+    console.log(this.form);
+  }
 }
